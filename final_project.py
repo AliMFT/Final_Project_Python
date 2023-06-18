95cc95d98ec19ee7fcde85d3df9c23842605026f
AliMFT <alitotaha5@gmail.com>
2023-06-18 20:49:47 +0300

    TODO 4

--- final_project.py
+++ final_project.py
@@ -25,8 +25,14 @@ class Student:
     # student_age (user input)
     # student_number (user_input)
     # courses_list (List of Course Objects)
-    def __init__(self):
-        pass
+    def __init__(self, student_name, student_age, student_number):
+        self.student_id = str(uuid.uuid4())
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = []  # List of Course Objects
+        Student.total_students += 1
+
 
     # TODO 5 define a method to enroll new course to student courses list
 
@@ -43,6 +49,8 @@ class Student:
     def get_student_average(self):
         # TODO 7 return the student average
         pass
+
+
 
 # in Global Scope
 # TODO 8 declare empty students list
