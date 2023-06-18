97f7d490f44e00d816f75086bae13aa02439f0d7
AliMFT <alitotaha5@gmail.com>
2023-06-18 20:44:22 +0300

    TODO 2 Define Course Class and define constructor with

--- final_project.py
+++ final_project.py
@@ -10,6 +10,11 @@ Delivery Date : 18/6/2023
 # course_id (generated using uuid4) ,
 # course name (user_input) and
 # course mark (user_input)
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = str(uuid.uuid4())
+        self.course_name = course_name
+        self.course_mark = course_mark
 
 class Student:
     # TODO 3 define static variable indicates total student count
